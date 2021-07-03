91abb5ecc2a54b30495dbb6d37e6abb690b97088
Peter Chatain <pchatain@stanford.edu>
2021-07-03 15:09:13 +0200

    added animate property to  classical wire VGroup

--- SQCA_Video/scene.py
+++ SQCA_Video/scene.py
@@ -43,36 +43,86 @@ class prereq(Scene):
   #   self.wait(1)
   #   self.play(Write(on_screen_time))
 
-class classical_circuit(Scene):
+class helpTest(Scene):
   def construct(self):
+    dot = Dot([-4, 0, 0])
+    d1_text = Text("0").next_to(dot, UP).scale(0.7)
+
 
     bit_group1 = VGroup()
     bit_group1.add(*Dot([-4, 0, 0]))
-    bit_group2 = VGroup().add(*Dot([-4, -1, 0]))
-    dot = Dot([-4, 0, 0])
-    dot2 = Dot([-4, -1, 0])
-    bit_group1.add(*Text("0").next_to(dot, UP).scale(0.7))
-    bit_group2.add(*Text("1").next_to(dot2, UP).scale(0.7))
-    self.play(Write(bit_group1))
-    self.play(Write(bit_group2))
-    self.wait(1)
-    d1_text = Text("0").next_to(dot, UP).scale(0.7)
-    d2_text = Text("1").next_to(dot2, UP).scale(0.7)
 
-    time = 0.0
+    bit_group1.add(*Text("0").next_to(dot, UP).scale(0.7))
+    line1 = Line([-4, 0, 0], [4, 0, 0])
+    self.add(line1)
+    self.play(MoveAlongPath(bit_group1, line1), run_time=2, rate_func=linear)
+    points = d1_text.get_points()
+    print(len(points))
+    print("length of points ^")
+    circ = Circle().next_to(points[len(points)/2])
+    self.add(circ)
+    self.wait(1)
+
+class classical_circuit(Scene):
+  def construct(self):
+
+    dot1 = Dot([-4, 0, 0])
+    dot2 = Dot([-4, -1, 0])
+    
     line1 = Line([-4, 0, 0], [4, 0, 0])
     line2 = Line([-4, -1, 0], [4, -1, 0])
+    dot1.move_to(line1.get_start())
+    dot2.move_to(line2.get_start())
+    text1=Tex("0").next_to(dot1, UP).scale(0.7)
+    text2=Tex("1").next_to(dot2, UP).scale(0.7)
+
+    bit_group1 = VGroup(dot1, text1)
+    bit_group2 = VGroup(dot2, text2)
+    self.play(Create(line1), Create(line2))
+    self.play(Write(bit_group1), Write(bit_group2))
+
+    t1 = ValueTracker(line1.get_start()[0])
+    t2 = ValueTracker(line2.get_start()[0])
+    time = 0.0
     on_screen_time = Variable(time, Text("t"), num_decimal_places=2).shift(UP)
-    self.play(Write(line1), Write(line2))
-    self.wait(1)
     self.play(Write(on_screen_time))
-
-    t2 = 2.0
     time_tracker = on_screen_time.tracker
-    self.wait(2)
+    time2 = 2.0
+    bit_group1.add_updater(lambda x: x.set_x(t1.get_value()))
+    bit_group2.add_updater(lambda x: x.set_x(t2.get_value()))
 
-    self.play(MoveAlongPath(bit_group1, line1), MoveAlongPath(bit_group2, line2), Transform(d1_text, d1_text_moved), Transform(d2_text, d2_text_moved), time_tracker.animate.set_value(t2), run_time=2, rate_func=linear)
-    self.wait()
+    self.play(t1.animate.set_value(line1.get_end()[0]), t2.animate.set_value(line2.get_end()[0]), time_tracker.animate.set_value(time2),rate_func=linear,run_time=2.0)
+ 
+
+    # dot1 = Dot([-4, 0, 0])
+    # dot2 = Dot([-4, -1, 0])
+    # bit_group1 = VGroup()
+    # bit_group1.add(*Dot([-4, 0, 0]))
+    # bit_group2 = VGroup().add(*Dot([-4, -1, 0]))
+
+
+    # bit_group1.add(*Text("0").next_to(dot, UP).scale(0.7))
+    # bit_group2.add(*Text("1").next_to(dot2, UP).scale(0.7))
+    # self.play(Write(bit_group1))
+    # self.play(Write(bit_group2))
+    # self.wait(1)
+    # d1_text = Text("0").next_to(dot, UP).scale(0.7)
+    # d2_text = Text("1").next_to(dot2, UP).scale(0.7)
+
+    # time = 0.0
+    # line1 = Line([-4, 0, 0], [4, 0, 0])
+    # line2 = Line([-4, -1, 0], [4, -1, 0])
+    # on_screen_time = Variable(time, Text("t"), num_decimal_places=2).shift(UP)
+    # self.play(Write(line1), Write(line2))
+    # self.wait(1)
+    # self.play(Write(on_screen_time))
+
+    # t2 = 2.0
+    # time_tracker = on_screen_time.tracker
+    # self.wait(2)
+
+    # self.play(MoveAlongPath(bit_group1, line1), MoveAlongPath(bit_group2, line2), Transform(d1_text, d1_text_moved), Transform(d2_text, d2_text_moved), time_tracker.animate.set_value(t2), run_time=2, rate_func=linear)
+    # self.wait()
 
 class wire_classical(Scene):
   def construct(self):
@@ -104,4 +154,46 @@ class wire_classical(Scene):
     self.wait(2)
 
     self.play(MoveAlongPath(d1, line1), MoveAlongPath(d2, line2), Transform(d1_text, d1_text_moved), Transform(d2_text, d2_text_moved), time_tracker.animate.set_value(t2), run_time=2, rate_func=linear)
+    self.wait()
+
+def bit(dot, n1, n2):
+  grp = VGroup()
+  grp.add(*dot)
+  crc = Circle()
+  d1 = Dot(n1)
+  d2 = Dot(n2)
+  grp.add(Arrow(n1, n2))
+  return grp
+
+class quantum_bit(Scene):
+  def construct(self):
+
+    d1 = Dot([-4, 0, 0])
+    d2 = Dot([-4, -1, 0])
+
+
+
+    arrow = Arrow(d1, d2)
+    self.play(Write(arrow))
+
+    d1_text = Text("0").next_to(d1, UP).scale(0.7)
+    d2_text = Text("1").next_to(d2, UP).scale(0.7)
+    d1_qubit = Arrow(DOWN, UP).next_to(d1, UP).scale(0.3)
+    d2_qubit = Arrow(UP, DOWN).next_to(d2, UP).scale(0.3)
+    self.play(Write(d1), Write(d2), Write(d1_text), Write(d2_text))
+    self.play(Transform(d1_text, d1_qubit), Transform(d2_text, d2_qubit))
+
+    grp1 = VGroup(d1, d1_text)
+    grp1.add(*dot)
+    crc = Circle()
+    d1 = Dot(n1)
+    d2 = Dot(n2)
+    grp.add(Arrow(n1, n2))
+
+
+
+class image_test(Scene):
+  def construct(self):
+    image = ImageMobject("test_image").scale(0.5)
+    self.play(FadeIn(image))
     self.wait()
